77fb61ad097e29575caa9e2cbe656180229adc12
yasser <yassermohsen101010@gmail.com>
2017-05-03 18:58:48 +0200

    product picture validation and view

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_products #, :dependent => destroy
   has_many :orders, through: :order_products
-  has_attached_file :picture
-  validates_attachment :picture,
-                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  has_attached_file :picture,
+                    styles: { thumb: ["64x64#", :png]}
+  validates_attachment :picture,  presence: true,
+                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+                       size: { in: 0..500.kilobytes }
 end
