92cee8192f2d1943dd0aa26c5d805b59452982b8
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-05-03 19:04:03 +0200

    No one can open admin except admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,13 @@
 class Admin::AdminController < ActionController::Base
   layout 'admin'
   protect_from_forgery with: :exception
+  before_filter :authenticate_user!
+  before_action :is_admin?
   semantic_breadcrumb "Admin", :admin_path
+
+  def is_admin?
+    unless current_user.is_admin
+      redirect_to root_path, notice: 'Access denied.'
+    end
+  end
 end
