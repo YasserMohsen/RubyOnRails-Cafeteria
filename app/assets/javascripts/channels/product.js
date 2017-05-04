fadb13839253b4bd6413efc5de8915fd667b1275
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-05-04 20:17:46 +0200

    Finsh New Order ActionCable + Js Implementation

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,31 +1,109 @@
-var cart_products = {};
+var refreshData = function () {
+    // this. refer to Cart scope, because i used callback.call(this)
+    $("#cart").html(this.render());
+    $("#total").text(this.getTotal())
+};
+
+var Cart = function () {
+    this.list = {}
+};
+
+Cart.prototype.add = function (product,callback) {
+    if (product.id in this.list) {
+        this.list[product.id].quantity += 1;
+    } else {
+        this.list[product.id] = {quantity: 1, product: product};
+    }
+    if (callback && typeof(callback) === "function") {
+        callback.call(this);
+    }
+};
+
+Cart.prototype.update = function (id, new_product, callback) {
+    if (id in this.list) {
+        this.list[id].product = new_product;
+    }
+    if (callback && typeof(callback) === "function") {
+        callback.call(this);
+    }
+};
+
+Cart.prototype.remove = function (id,callback) {
+    if (id in this.list) {
+        delete this.list[id];
+    }
+    if (callback && typeof(callback) === "function") {
+        callback.call(this);
+    }
+};
+
+Cart.prototype.getTotal = function () {
+    var total = 0;
+    for (var product in this.list) {
+        total += this.list[product].quantity * this.list[product].product.price;
+    }
+    return total;
+};
+
+Cart.prototype.render = function () {
+    html = "";
+    if (Object.keys(this.list).length === 0) {
+        html += "<tr><td colspan='4'>Empty ...</td></tr>";
+    }else{
+        for (var product in this.list) {
+            html += "<tr>";
+            html += "<td>" + this.list[product].product.name + "</td>";
+            html += "<td>" + this.list[product].quantity + "</td>";
+            html += "<td>" + this.list[product].product.price + "</td>";
+            html += "<td><i data-id='" + product +"' class='large remove red icon remove_line'></i></td>";
+            html += "</tr>";
+        }
+    }
+    return html;
+};
+
+var cart = new Cart();
 
 App.product = App.cable.subscriptions.create("ProductChannel", {
     connected: function () {
         console.log("connected")
     },
-
     disconnected: function () {
         console.log("disconnected")
     },
-
     received: function (data) {
-        console.log(data);
-        if(data.action === "create"){
-            console.log("#category_" + data.product.category_id)
+        console.log(data)
+        if (data.action === "create") {
             $("#category_" + data.product.category_id).append(data.html);
-        }else if (data.action === "destroy"){
+        } else if (data.action === "update") {
+            if (data.product.availability === false) {
+                $("#product_" + data.product.id).remove();
+                cart.remove(data.product.id,refreshData);
+            } else {
+                $("#product_" + data.product.id).replaceWith(data.html);
+                cart.update(data.product.id,data.product,refreshData);
+            }
+        } else if (data.action === "destroy") {
             $("#product_" + data.product.id).remove();
+            cart.remove(data.product.id,refreshData);
         }
     }
 });
 
-$(document).on("click", ".add-to-cart", function () {
-    var button = $(this);
-    var product = {
-        id : button.data("id"),
-        category_id : button.data("category_id"),
-        price : button.data("price")
-    };
-    console.log(product)
+
+$(document).ready(function () {
+    $(document).on("click", ".add-to-cart", function () {
+        var button = $(this);
+        var product = {
+            id: button.data("id"),
+            name: button.data("name"),
+            category_id: button.data("category_id"),
+            price: button.data("price")
+        };
+        cart.add(product,refreshData);
+    });
+    $(document).on('click','.remove_line',function () {
+        var product_id = $(this).data("id");
+        cart.remove(product_id,refreshData);
+    })
 });
