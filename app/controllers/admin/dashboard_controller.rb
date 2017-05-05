258c36ee5a98c3d481e6d9d3dfb2cc4ee225d47d
yasser <yassermohsen101010@gmail.com>
2017-05-05 10:40:50 +0200

    updates in dashboard and order model

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < Admin::AdminController
   def index
-
+    @orders = Order.where(checked: false)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  #has_many :order_products, :dependent => destroy
+  has_many :order_products, :dependent => :destroy
   has_many :products, through: :order_products
+
+  after_initialize :set_defaults, unless: :persisted?
+  def set_defaults
+    self.status  ||= 'received'
+    self.checked = false if self.checked.nil?
+  end
 end
