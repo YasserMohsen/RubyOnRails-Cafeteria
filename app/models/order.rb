22cdc15235429a81ac01eb1d27e85245025611f2
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-05-05 10:39:37 +0200

    Add Checks Controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  #has_many :order_products, :dependent => destroy
+  has_many :order_products, :dependent => :destroy
   has_many :products, through: :order_products
+
+  def get_total
+    self.products.sum("amount * price").inspect
+  end
+
 end
