7a4bce18ea815c584e8a8ec2c1d6347ad153ddb3
salmabadr <salmabadr00@gmail.com>
2017-05-05 12:11:52 +0200

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,38 @@
 class OrdersController < ApplicationController
   def index
+    # @orders=Order.where(:user_id=>current_user.id)
+    @orders=Order.where(user_id:current_user.id)
+    @costs=Hash.new
+    @products=Array.new
+    Array(@orders).each do |order|
+        total=0
+        prodids=Array.new
+        prodamounts=OrderProduct.where(order_id:order['id']).select([:product_id,:amount])
+        prodamounts.each do |product|
+          prodids.push(product.product_id)
+        end
+        prods=Product.where(id: prodids).select([:id,:price,:name])
+        prods.each do |pr|
+        prodamounts.each do |am|
+            if am.product_id==pr.id
+              amount=am.amount*pr.price
+              total+=amount
+
+              pr.class_eval do
+                attr_accessor :amount
+              end
+              pr.amount=am.amount
+
+            end
+          end
+        end
+        @products.push(prods)
+        @costs[order.id]=total
+    end
+
+
+
+    # @orders=Order.
   end
   def new
     @order = Order.new
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :room
   #has_many :order_products, :dependent => destroy
   has_many :products, through: :order_products
+   after_update_commit { UserOrdersJob.perform_later self }
 end
